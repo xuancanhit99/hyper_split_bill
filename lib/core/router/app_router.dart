9a665d29053b0487221794ef54e01156a1131f27
xuancanhit99 <xuancanhit99@gmail.com>
2025-04-07 08:05:39 +0300

    Remove unused import in app_router.dart

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,7 +2,6 @@
 
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hyper_split_bill/features/auth/presentation/bloc/auth_bloc.dart'; // Import AuthBloc
